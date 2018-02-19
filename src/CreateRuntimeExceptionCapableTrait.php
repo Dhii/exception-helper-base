370495ae8a77f56ab8a2fe519cfed31ccf938777
Anton Ukhanev <xedin.unknown@gmail.com>
2018-02-19 13:51:35 +0100

    Updated param types for `RuntimeException` factory

--- src/CreateRuntimeExceptionCapableTrait.php
+++ src/CreateRuntimeExceptionCapableTrait.php
@@ -17,9 +17,9 @@ trait CreateRuntimeExceptionCapableTrait
      *
      * @since [*next-version*]
      *
-     * @param string|Stringable|null $message  The error message, if any.
-     * @param int|null               $code     The error code, if any.
-     * @param RootException|null     $previous The inner exception for chaining, if any.
+     * @param string|Stringable|int|float|bool|null $message  The message, if any.
+     * @param int|float|string|Stringable|null      $code     The numeric error code, if any.
+     * @param RootException|null                    $previous The inner exception, if any.
      *
      * @return RuntimeException The new exception.
      */
